6a2165b639f22bf18e349e6741f2effddcd3c42e
Pavan-tibil <pavan.kumar@tibilsolutions.com>
2024-06-06 11:11:35 +0530

    change the code to read the credentials from .env file

--- query_data.py
+++ query_data.py
@@ -1,12 +1,24 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Database credentials
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
 # Connect to PostgreSQL database using psycopg2
 conn = psycopg2.connect(
-    dbname="maiora",
-    user="postgres",
-    password="Pavan@97",
-    host="localhost",
-    port="5432"
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
 )
 cursor = conn.cursor()
 
--- upload_data.py
+++ upload_data.py
@@ -1,6 +1,21 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 import urllib.parse
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Database credentials
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+# URL-encode the password
+password = urllib.parse.quote(DB_PASSWORD)
 
 # Load the data from the provided CSV files
 file_a = '/home/pavan/Downloads/order_region_a.csv'
@@ -23,11 +38,8 @@ combined_df['total_sales'] = combined_df['QuantityOrdered'] * combined_df['ItemP
 # Remove duplicate entries based on 'OrderId'
 combined_df = combined_df.drop_duplicates(subset='OrderId')
 
-# URL-encode the password
-password = urllib.parse.quote('Pavan@97')
-
 # Connect to PostgreSQL database
-engine = create_engine(f'postgresql+psycopg2://postgres:{password}@localhost:5432/maiora')
+engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{password}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 
 # Load the transformed data into the sales_data table
 combined_df.to_sql('sales_data', engine, if_exists='replace', index=False, method='multi')
